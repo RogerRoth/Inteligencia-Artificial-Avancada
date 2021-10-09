ba71147e68b9df6ccd64d0c94777dc08fad2183c
RogerRoth <rogr_roth@yahoo.com.br>
2021-10-09 16:55:36 -0300

    Create to read dll

--- Trabalho1/main.py
+++ Trabalho1/main.py
@@ -1,29 +1,28 @@
 import ctypes
+from inspect import getmembers, isclass, isfunction
 from typing import NewType
 import clr
 import sys
+
+
 sys.path.append(r"F:\Roger\Faculdade\Inteligencia Artificial Avançada\Trabalho1\AdrenaDLL")
 clr.AddReference("ADReNA_API")
 
-import ADReNA_API
+from ADReNA_API.NeuralNetwork import Backpropagation
+from ADReNA_API.Data import DataSet, DataSetObject
 
-#back = py_object(ADReNA_API)
+neural = Backpropagation(2, 1)
 
+trainingSet = DataSet(2,1)
 
+# Ajustar treinamento, são dois parametros de arrays de double
+trainingSet.Add(DataSetObject( {0,0}, {0} ))
+trainingSet.Add(DataSetObject( {0,1}, {1} ))
+trainingSet.Add(DataSetObject( {1,0}, {1} ))
+trainingSet.Add(DataSetObject( {1,1}, {0} ))
 
-my_func = getattr(ADReNA_API)
-
-my_func()
-
-
-
-print(back)
-
-#adrenaDll = ctypes.WinDLL ("AdrenaDLL\\ADReNA_API.dll")
-#newDll = ctypes.WinDLL ("AdrenaDLL\\Newtonsoft.Json.dll")
+neural.learn(trainingSet)
 
 
-#adrenaDll = cdll.LoadLibrary("AdrenaDLL\\ADReNA_API.dll")
-
-#backpropagation = adrenaDll.
-
+#Para verificar as funções da classe
+#print(getmembers(neural))
